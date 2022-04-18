0be50d9f8aab9f6b1f7eea056d9e64d8473b934e
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2022-04-18 09:32:25 -0300

    Adiciona todos os exercicios resolvidos da lista de alocacao dinamica

--- Aula_Pratica/Aula8/exercicio.c
+++ Aula_Pratica/Aula8/exercicio.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 // Exercicio 1
-void juntar(int *x1, int n1, int *x2, int n2, int *x3) {
+/* void juntar(int *x1, int n1, int *x2, int n2, int *x3) {
   int qtd, dif = 0, *xtemp;
 
   printf("Elementos X1:\n");
@@ -74,88 +74,95 @@ int main() {
   juntar(x1, n1, x2, n2, x3);
 
   return 0;
-}
+} */
 
 // Exercicio 2
-/* #include <stdio.h>
+/* void intersec(int n1, int n2, int *x1, int *x2, int *x3) {
+  int qtd, dif = 0, *xtemp;
 
-int interseccao(int *x1, int *x2, int *x3, int n1, int n2, int n3) {
-  int i = 0, j = 0, k = 0;
-  int alterados = 0;
+  printf("Elementos X1:\n");
+  for (int i = 0; i < n1; i++) {
+    scanf("%d", x1 + i);
+  }
 
-  for (i = 0; i < n1; i++) {
-    for (j = 0; j < n2; j++) {
-      if (x2[j] == x1[i]) {
-        alterados += 1;
-        x3[k] = x1[i];
-        k += 1;
-      }
+  printf("Elementos X2:\n");
+  for (int i = 0; i < n2; i++) {
+    scanf("%d", x2 + i);
+  }
+
+  for (int i = 0; i < n1; i++) {
+    if (*(x1 + i) == *(x2 + i)) {
+      dif++;
     }
   }
 
-  k = 0;
+  qtd = dif;
+  x3 = malloc(qtd * sizeof(int));
+  xtemp = malloc(n1 + n2 * sizeof(int));
 
-  for (i = 0; i < alterados; i++) {
-    for (j = i + 1; j < alterados;) {
-      if (x3[j] == x3[i]) {
-        for (k = j; k < alterados; k++)
-          x3[k] = x3[k + 1];
+  for (int i = 0; i < n1; i++) {
+    *(xtemp + i) = *(x1 + i);
+  }
+  for (int j = n1; j < (n1 + n2); j++) {
+    *(xtemp + j) = *(x2 + (j - n1));
+  }
 
-        alterados--;
+  for (int i = 0; i < (n1 + n2); i++) {
+    for (int z = 0; z < qtd; z++) {
+      if (i == z) {
+        if (*(xtemp + i) == *(x3 + z)) {
+         // *(x3 + z) = *(xtemp + i);
+        } else {
+          *(x3 + z) = *(xtemp + i);
+        }
+        break;
       } else {
-        j++;
+        if (*(xtemp + i) == *(x3 + z)) {
+          *(x3 + z) = 0;
+          *(x3 + z) = *(xtemp + i);
+          break;
+        } else if (*(x3 + z) == 0) {
+         *(x3 + z) = *(xtemp + i);
+          break;
+        }
       }
     }
   }
-  return alterados;
+
+  for (int i = 0; i < qtd; i++) {
+    printf("x3 irá conter: %d\n", *(x3 + i));
+  }
+  printf("%d\n", qtd);
 }
 
-int main(void) {
-  int n1, n2, n3;
-  int i = 0, alterados;
+int main() {
+  int *x1, *x2, *x3;
+  int n1, n2;
 
-  printf("Tamanho x1\n");
+  printf("Insira a quantidade valores do vetor x1:\n");
   scanf("%d", &n1);
-
-  printf("Tamanho x2\n");
+  printf("Insira a quantidade valores do vetor x2:\n");
   scanf("%d", &n2);
 
-  if (n1 > n2) {
-    n3 = n1;
-  } else {
-    n3 = n2;
-  }
+  x1 = malloc(n1 * sizeof(int));
+  x2 = malloc(n2 * sizeof(int));
 
-  int x1[n1], x2[n2], x3[n3];
-
-  printf("Vetor 1\n");
-  for (i = 0; i < n1; i++) {
-    scanf("%d", &x1[i]);
-  }
-
-  printf("Vetor 2\n");
-  for (i = 0; i < n2; i++) {
-    scanf("%d", &x2[i]);
-  }
-
-  alterados = interseccao(x1, x2, x3, n1, n2, n3);
-
-  printf("%d alterações e Vetor x3 alterado\n", alterados);
-
-  for (i = 0; i < alterados; i++) {
-    printf("%d ", x3[i]);
-  }
+  intersec(n1, n2, x1, x2, x3);
 
   return 0;
 } */
 
 // Exercicio 3 -> feito
-void readArray(int *v, int n) {
-  for (int i = 0; i < n; i++) {
+/* void readArray(int *v, int n) {
+  int i;
+
+  for (i = 0; i < n; i++) {
     scanf("%d", v + i);
   }
 
-  // return *v;
+  for (i = 0; i < n; i++) {
+    printf("%d ", *(v + i));
+  }
 }
 
 int main() {
@@ -167,15 +174,13 @@ int main() {
   v = malloc(n * sizeof(int));
 
   readArray(v, n);
-  for (int i = 0; i < n; i++) {
-    printf("%d\t", *(v+i));
-  }
+  
 
   return 0;
-}
+} */
 
 // Exercicio 4
-#define LIN 3
+/* #define LIN 3
 #define COL 3
 
 int main() {
@@ -232,4 +237,4 @@ int main() {
   free(result);
 
   return 0;
-}
+} */
